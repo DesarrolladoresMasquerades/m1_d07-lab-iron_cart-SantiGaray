89b492592c3fa1b6cd015b9ae78156a4e4ee977b
Santiago <santiagofgaray@gmail.com>
2022-01-26 07:27:29 +0100

    lab finished

--- js/index.js
+++ js/index.js
@@ -1,31 +1,61 @@
 // ITERATION 1
 
+// const { product } = require("puppeteer");
+
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  const price = product.querySelector(".price span").innerText
+  const quantity = product.querySelector(".quantity input").value
+  const subtotal  = price * quantity
+
+  const subTotalFinal = product.querySelector(".subtotal span")
+  subTotalFinal.innerText = subtotal
+
+  return subtotal
 }
+
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
 
   // ITERATION 2
-  //... your code goes here
+  const products = Array.from(document.getElementsByClassName("product"))
 
+  products.forEach((element) => {
+    updateSubtotal(element)
+  }
+  )
+  
   // ITERATION 3
   //... your code goes here
+
+  const total = products.reduce((ac,element)=>{ 
+    return ac += updateSubtotal(element)
+  },
+  0)
+ 
+  document.querySelector("#total-value span").textContent = total
+  return total
+
+
+
 }
+
 
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+
+  //console.log('The target in remove is:', target);
+
+  const removeFinal = (target.parentNode.parentNode).remove()
+
+  calculateAll()
+
+
+
+
 }
 
 // ITERATION 5
@@ -37,6 +67,12 @@ function createProduct() {
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
+
+  const removeElement = document.querySelectorAll(".btn-remove")
+  removeElement.forEach(element=>(
+  element.addEventListener("click", removeProduct)))
+
+  console.log(removeElement)
 
   //... your code goes here
 });
